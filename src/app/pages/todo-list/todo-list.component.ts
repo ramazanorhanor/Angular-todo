9ff462c53c92d693be4f1cdff6d5c25f78cd3032
ramazan.orhan <ramazan.orhan@tubitak.gov.tr>
2022-12-29 12:31:33 +0300

    todo 6

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+import { TodoServiceService } from 'src/app/services/todo-service.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-
-  constructor() { }
+todoList:Todo[]=[];
+  constructor(private todoService:TodoServiceService) { }
 
   ngOnInit(): void {
+this.load()
+  }
+  load(){
+    this.todoService.getAll().subscribe(x=>this.todoList=x)
+
+  }
+  delete(id:number){
+    this.todoService.delete(id).subscribe(x=>{
+      if(x==true){
+        alert("silme işlemi tamalandı")
+        this.load()
+      }
+      else{
+        alert("silinemedi başarısız")
+      }
+    })
+  }
+  isCompleted(id:number){
+    this.todoService.isCompleted(id).subscribe(x=> {
+      if(x==true){
+        let index=this.todoList.findIndex(x=>x.id==id)
+        this.todoList[index].isCompleted=!this.todoList[index].isCompleted;
+      }
+    })
   }
 
 }
--- src/app/services/todo-service.service.ts
+++ src/app/services/todo-service.service.ts
@@ -1,10 +1,33 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+import {map, Observable} from 'rxjs';
+import { TodoUpdate } from '../models/todo-update';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoServiceService {
+  constructor(
+    private http: HttpClient,
+    @Inject('Base_Api_Url') private baseUrl: string
+  ) {}
+  getAll() {
+    return this.http.get<Todo[]>(`${this.baseUrl}/api/todos`);
+  }
+  getById(id:number){
+   return  this.http.get<Todo>(`${this.baseUrl}/api/todos/${id}`)
+  }
 
-  constructor(private http:HttpClient) { }
+  add(todo:Todo):Observable<boolean>{
+    return this.http.post<Response>(`${this.baseUrl}/api/todos`,todo,{observe:'response'}).pipe(map((res:HttpResponse<Response>)=>res.status==201))
+  }
+  delete(id:number):Observable<boolean>{
+    return this.http.delete<Response>(`${this.baseUrl}/api/todos/${id}`,{observe:'response'}).pipe(map((res:HttpResponse<Response>)=>res.status==204))
+  }
+  update(todoUpdate:TodoUpdate){
+    return this.http.put<Response>(`${this.baseUrl}/api/todos`,todoUpdate,{observe:'response'}).pipe(map((res:HttpResponse<Response>)=>res.status==204))
+  }
+  isCompleted(id:number){
+    return this.http.put<Response>(`${this.baseUrl}/api/todos/${id}`,{},{observe:'response'}).pipe(map((res:HttpResponse<Response>)=>res.status==204))
+  }
 }
