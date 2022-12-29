f7d8969c16c025785afa38ab9cfe23d6dca750f7
ramazan.orhan <ramazan.orhan@tubitak.gov.tr>
2022-12-29 11:39:23 +0300

    todo 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { TodoAddComponent } from './pages/todo-add/todo-add.component';
 import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
-
+import {HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +17,10 @@ import { TodoListComponent } from './pages/todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide:'Base_Api_Url',useValue:environment.baseUrl}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
