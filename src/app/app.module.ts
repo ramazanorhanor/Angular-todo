3b62e353c297b66e99856c69d553634a79eb2dff
ramazan.orhan <ramazan.orhan@tubitak.gov.tr>
2022-12-29 10:16:58 +0300

    todo 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoAddComponent } from './pages/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent,
+    TodoUpdateComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
